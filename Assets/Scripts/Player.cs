e204f5e7ede55e3e4af27c3c7615b12c49b3ca61
Yoshito1204 <s2032142mr@s.chibakoudai.jp>
2022-09-29 17:21:33 +0900

    player_move, followcamera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,28 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-
+    public GameObject Camera;
     
     private GameObject fieldObject;
+    private float speed;
+    private Animator anim = null;
+     private Rigidbody2D rb = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+         rb = GetComponent<Rigidbody2D>();
         fieldObject = GameObject.Find("Canvas");
         fieldObject.SetActive(false);
+        speed = 3f;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+       Camera.transform.position = new Vector3(transform.position.x,transform.position.y,transform.position.z - 10);
+
         if(Input.GetKey(KeyCode.Return)){
              fieldObject.SetActive(false);
              
@@ -29,6 +35,20 @@ public class Player : MonoBehaviour
             fieldObject.SetActive(true);
         }
             
-        
+        Move();
+    }
+
+    public void Move(){
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector3 IdouHoukou = new Vector3(x, y, 0);
+
+        //rbに力を加える
+        //velocityは質量などの物理演算を無視するときに使う
+        rb.velocity = IdouHoukou * speed;
+
+         //rbの回転をさせない様にする
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 }
